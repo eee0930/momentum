578278dd4089004dc4f73b6acaee9833d90239c3
lazyhysong <37135523+eee0930@users.noreply.github.com>
2022-02-21 01:49:35 +0900

    completed, logout 추가

--- js/greetings.js
+++ js/greetings.js
@@ -2,6 +2,7 @@ const loginDiv = document.querySelector(".nameDiv");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const logoutBtn = document.querySelector("#logout");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -17,6 +18,15 @@ function onLoginSubmit(event){
 function paintGreetings(username) {
     greeting.innerText = `Hello, ${username}.`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    logoutBtn.classList.remove(HIDDEN_CLASSNAME);
+    logoutBtn.addEventListener("click", handleLogout);
+}
+
+function handleLogout() {
+    localStorage.removeItem(USERNAME_KEY);
+    loginDiv.classList.remove(HIDDEN_CLASSNAME);
+    logoutBtn.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = "";
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
--- js/todo.js
+++ js/todo.js
@@ -1,21 +1,63 @@
 const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
+const checkedToDoList = document.querySelector("#checked-todo-list");
+const checkedList = document.querySelector(".checked-list");
+const clearBtn = document.querySelector("#clearBtn");
+
+const HIDDEN_CLASS = "hidden";
+const MAX_CHECK_SIZE = 5;
+const SAVE_TODOS = "toDos";
+const SAVE_CHECK_TODOS = "checkedToDos";
 
 let toDos = [];
+let checkedToDos = [];
 
 function saveToDos() {
-    localStorage.setItem("toDos", JSON.stringify(toDos));
+    localStorage.setItem(SAVE_TODOS, JSON.stringify(toDos));
+}
+function saveCheckedToDos(text) {
+    if(checkedToDos.length === MAX_CHECK_SIZE) {
+        checkedToDos.shift();
+        const first = document.querySelector("#checked-todo-list li:first-child");
+        first.remove();
+    }
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const dateNum = String(date.getDate()).padStart(2, "0");
+    const checkedToDo = {
+        text: text,
+        date: `${year}-${month}-${dateNum}`
+    }
+    checkedToDos.push(checkedToDo);
+    localStorage.setItem(SAVE_CHECK_TODOS, JSON.stringify(checkedToDos));
+    paintCheckedToDo(checkedToDo);
 }
 
 function deleteTodo(event) {
     const li = event.target.parentElement;
+    toDos = toDos.filter(todo => todo.id !== parseInt(li.id));
+    li.remove();
+    saveToDos();
+}
+
+function checkTodo(event) {
+    const li = event.target.parentElement;
+    const text = li.children[1].innerText;
     li.classList.add("checked")
     setTimeout(function() {
         li.classList.add("clear");
+        deleteTodo(event);
     }, 1800);
-    toDos = toDos.filter(todo => todo.id !== parseInt(li.id));
-    saveToDos();
+    saveCheckedToDos(text);
+}
+
+function removeAllCheckedToDos() {
+    checkedToDos = [];
+    checkedToDoList.innerHTML = "";
+    localStorage.removeItem(SAVE_CHECK_TODOS);
+    checkedList.classList.add(HIDDEN_CLASS);
 }
 
 /**
@@ -27,11 +69,29 @@ function paintToDo(newTodo) {
     const span = document.createElement("span");
     li.id = newTodo.id;
     span.innerText = newTodo.text;
-    const button = document.createElement("button");
-    button.addEventListener("click", deleteTodo);
-    li.appendChild(button);
+    const checkBtn = document.createElement("button");
+    checkBtn.addEventListener("click", checkTodo);
+    checkBtn.classList.add("check-btn");
+    const delBtn = document.createElement("button");
+    delBtn.addEventListener("click", deleteTodo);
+    delBtn.classList.add("del-btn");
+    delBtn.innerText = "X";    
+    li.appendChild(checkBtn);
     li.appendChild(span);
+    li.appendChild(delBtn);
     toDoList.appendChild(li);
+}
+
+function paintCheckedToDo(checkedTodo) {
+    checkedList.classList.remove(HIDDEN_CLASS);
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = checkedTodo.text;
+    const dateSpan = document.createElement("span");
+    dateSpan.innerText = checkedTodo.date;
+    li.appendChild(span);
+    li.appendChild(dateSpan);
+    checkedToDoList.appendChild(li);
 }
 
 /**
@@ -54,10 +114,18 @@ function handleToDoSubmit(event) {
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 
-const savedTodos = localStorage.getItem("toDos");
+const savedTodos = localStorage.getItem(SAVE_TODOS);
+const savedCheckedTodos = localStorage.getItem(SAVE_CHECK_TODOS);
 
 if(savedTodos) {
     const parsedTodos = JSON.parse(savedTodos);
     toDos = parsedTodos;
     parsedTodos.forEach(paintToDo);
 }
+if(savedCheckedTodos) {
+    const saveCheckedTodos = JSON.parse(savedCheckedTodos);
+    checkedToDos = saveCheckedTodos;
+    saveCheckedTodos.forEach(paintCheckedToDo);
+}
+
+clearBtn.addEventListener("click", removeAllCheckedToDos);
